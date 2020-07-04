4d310fb6d0bad9c6f1f5faadb0b06f96182f9498
Yi Lin Lee <yilinlee19@gmail.com>
2020-07-05 01:31:47 +1000

    update portfolio cards

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
-$(window).on("scroll", function(){
-    if($("body").scrollTop() === 50){
-      $(window).off("scroll");
+// $(window).on("scroll", function(){
+//     if($("body").scrollTop() === 500){
+//       $(window).off("scroll");
 
-      console.log("scrolled!");
-      // Do some stuff here ..
-    }
-})
+//       console.log("scrolled!");
+//       // Do some stuff here ..
+//     }
+// })
 
+// setTimeout(function() {
+//   console.log( $("body").scrollTop())
+// }, 5000)
+
+
+// Hover event over portfolio images
+// $(".portfolioImg").on("mouseover", function() {
+//   const hoverVidID = $(this).closest("a").attr("id");
+//   const hoverVidSrc = `./assets/imgs/porfolioVid${hoverVidID}.mov`;
+//   const hoverVid = $("<video autoplay loop class='hoverVid'>");
+//   hoverVid.append(`<source src="./assets/imgs/portfolioVid1.mov" />`);
+//   $(this).closest("a").append(hoverVid);
+// });
+
+// Handle mouse enter event on portfolio images to show zoomed out screenshot
+const handlerInPortfolio = function(event) {
+  const currentSrc = $(event.currentTarget).attr("src");
+  const newSrc = currentSrc.slice(0, -4) + "small.png";
+  $(event.currentTarget).attr("src", newSrc);
+}
+
+// Handle mouse leave event on portfolio images to show zoomed in screenshot
+const handlerOutPorfolio = function(event) {
+  const currentSrc = $(event.currentTarget).attr("src");
+  const newSrc = currentSrc.slice(0, -9) + ".png";
+  $(event.currentTarget).attr("src", newSrc);
+}
+
+// Hover event on portfolio images
+$(".portfolioImg").hover(handlerInPortfolio, handlerOutPorfolio);
