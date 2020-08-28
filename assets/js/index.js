c082c20345e8700c36a8ed89454ec150da380588
Yi Lin Lee <yilinlee19@gmail.com>
2020-08-28 17:54:33 +1000

    expand portfolio title

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,16 +6,17 @@ $(document).ready(function() {
         scroll_pos = $(this).scrollTop();
 
         // When scrolling from intro to portfolio, dynamically change..
-        if(scroll_pos > 500) {
-            // background color
+        if(scroll_pos > 900){
             $("body").css("background-color", "rgb(68, 0, 255)");
+        } else if(scroll_pos > 500) {
+            // background color
+            $("body").css("background-color", "rgb(111, 59, 255)");
             // and navbar colors
             $("#navbar-brand").css("text-shadow", "2px 3px black");
             $("#navbar-brand").css("-webkit-text-stroke-color", "black");
             $(".fab, .fas").css("color", "white");
             $(".navbar-nav, .nav-pages").css("color", "white")
-        }
-        else {
+        } else {
             $("body").css("background-color", "white");
             $("#navbar-brand").css("text-shadow", "2px 3px rgb(68, 0, 255)");
             $("#navbar-brand").css("-webkit-text-stroke-color", "rgb(68, 0, 255)");
