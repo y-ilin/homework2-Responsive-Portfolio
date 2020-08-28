e7fc8c8cebdd1dfa42f33ae1d19dab883bbf74c9
Yi Lin Lee <yilinlee19@gmail.com>
2020-08-28 22:22:09 +1000

    update styling

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,11 +6,11 @@ $(document).ready(function() {
         scroll_pos = $(this).scrollTop();
 
         // When scrolling from intro to portfolio, dynamically change..
-        if(scroll_pos > 900){
+        if(scroll_pos > 1000){
             $("body").css("background-color", "rgb(68, 0, 255)");
         } else if(scroll_pos > 500) {
             // background color
-            $("body").css("background-color", "rgb(111, 59, 255)");
+            $("body").css("background-color", "rgba(145, 133, 255, 0.637)");
             // and navbar colors
             $("#navbar-brand").css("text-shadow", "2px 3px black");
             $("#navbar-brand").css("-webkit-text-stroke-color", "black");
@@ -18,7 +18,7 @@ $(document).ready(function() {
             $(".navbar-nav, .nav-pages").css("color", "white")
         } else {
             $("body").css("background-color", "white");
-            $("#navbar-brand").css("text-shadow", "2px 3px rgb(68, 0, 255)");
+            $("#navbar-brand").css("text-shadow", "2px 2px rgb(68, 0, 255)");
             $("#navbar-brand").css("-webkit-text-stroke-color", "rgb(68, 0, 255)");
             $(".fab, .fas").css("color", "rgb(68, 0, 255)");
             $(".navbar-nav, .nav-pages").css("color", "rgb(68, 0, 255)")
