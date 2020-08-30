9f95d836eac9d7caa0694e7809fe409b91cd6294
Yi Lin Lee <yilinlee19@gmail.com>
2020-08-30 23:17:23 +1000

    update styling

--- assets/js/index.js
+++ assets/js/index.js
@@ -16,13 +16,14 @@ $(document).ready(function() {
             $("#navbar-brand").css("-webkit-text-stroke-color", "black");
             $(".fab, .fas").css("color", "white");
             $(".navbar-nav, .nav-pages").css("color", "white")
+            $(".navbar-nav .nav-item.nav-link.nav-pages.active").css("border-bottom", "1px solid white");
         } else {
             $("body").css("background-color", "rgb(219, 214, 255)");
             $("#navbar-brand").css("text-shadow", "2px 2px rgb(68, 0, 255)");
             $("#navbar-brand").css("-webkit-text-stroke-color", "rgb(68, 0, 255)");
             $(".fab, .fas").css("color", "rgb(68, 0, 255)");
             $(".navbar-nav, .nav-pages").css("color", "rgb(68, 0, 255)")
-            $(".navbar-nav .nav-item.nav-link.nav-pages.active").css("border-bottom", "1px solid ");
+            $(".navbar-nav .nav-item.nav-link.nav-pages.active").css("border-bottom", "1px solid rgb(68, 0, 255)");
         }
 
     });
