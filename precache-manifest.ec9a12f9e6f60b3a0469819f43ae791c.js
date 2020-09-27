2a4da0304e7492178e3f06e7cbaa6b2f24bca245
Yi Lin Lee <yilinlee19@gmail.com>
2020-09-27 23:51:20 +1000

    Updates

---
+++ precache-manifest.ec9a12f9e6f60b3a0469819f43ae791c.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edd68711e9f6955c376d871871f5573b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6339583aedc850b0b7c7",
+    "url": "/static/css/main.b4f924ff.chunk.css"
+  },
+  {
+    "revision": "fc91b6412717e8144871",
+    "url": "/static/js/2.4253f8af.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4253f8af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6339583aedc850b0b7c7",
+    "url": "/static/js/main.f4b69cfa.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "9661323f3ed937c2938d0e43fa7d6328",
+    "url": "/static/media/Burger.9661323f.png"
+  },
+  {
+    "revision": "963142cda48ffbe90dc181f714330752",
+    "url": "/static/media/CodingQuiz.963142cd.png"
+  },
+  {
+    "revision": "10a9255a4e44d3eb68292eaeea87343b",
+    "url": "/static/media/PersonalFitnessPlanner.10a9255a.png"
+  },
+  {
+    "revision": "492ca388f45f62128dd4b3b65d5a9787",
+    "url": "/static/media/WeatherDashboard.492ca388.png"
+  },
+  {
+    "revision": "8ae642ec13cd34a14055cd0456eb2670",
+    "url": "/static/media/WhiteboardSticky.8ae642ec.png"
+  },
+  {
+    "revision": "7fcc601cc1c2f087f1dc389a9e3afa1b",
+    "url": "/static/media/WorkDayScheduler.7fcc601c.png"
+  },
+  {
+    "revision": "55ad532a2d4639ba174f4e1367ea99d7",
+    "url": "/static/media/architect-Eucalyp.55ad532a.svg"
+  },
+  {
+    "revision": "1f832293258f214eaaa03b9a98ce8aab",
+    "url": "/static/media/data-Freepik.1f832293.svg"
+  },
+  {
+    "revision": "a505c345d0297f661db5f261626be815",
+    "url": "/static/media/integral-Freepik.a505c345.svg"
+  },
+  {
+    "revision": "acf7d82868985f5e94136ab361d2cb07",
+    "url": "/static/media/prototype-ultimatearm.acf7d828.svg"
+  },
+  {
+    "revision": "c2bc391d22ce8ceba5d5b211ed2dfc64",
+    "url": "/static/media/right-arrow-Freepik.c2bc391d.svg"
+  },
+  {
+    "revision": "421e4f2fdfdba92107597b1ef363ab91",
+    "url": "/static/media/startup-Freepik.421e4f2f.svg"
+  },
+  {
+    "revision": "76c17d87975b4fbd634639f073b1eb43",
+    "url": "/static/media/wind-turbine-Freepik.76c17d87.svg"
+  },
+  {
+    "revision": "8b995cfe2013336dde49d32a8f711794",
+    "url": "/static/media/yilin.8b995cfe.jpg"
+  }
+]);
